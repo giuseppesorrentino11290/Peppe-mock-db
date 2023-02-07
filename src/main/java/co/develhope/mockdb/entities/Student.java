ced25bf15798085e50e99ed5557669362e020b8f
giuseppe sorrentino <giuseppesorrentino11290@gmail.com>
2023-02-07 17:37:33 +0100

    Initial commit

---
+++ src/main/java/co/develhope/mockdb/entities/Student.java
@@ -0,0 +1,23 @@
+package co.develhope.mockdb.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "Students")
+public class Student{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+}
